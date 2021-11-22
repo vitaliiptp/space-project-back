c76ae59f90604bf413b948ea07a6c0b403f0fb9d
vitaliiptp <vitalii.potapenko@gmail.com>
2021-11-22 12:37:08 +0100

    added cors

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-// const cors = require("cors");
+const cors = require("cors");
 const { setupRoutes } = require('./routes');
 const connection = require("./db-config");
 
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
 setupRoutes(app);
 
